d010d3cb9cec941cc62aa83577f7088918482add
Joel Piper <joelmpiper@gmail.com>
2016-09-27 15:00:48 -0400

    Further app functionality

--- apps/identifyabill/views.py
+++ apps/identifyabill/views.py
@@ -35,23 +35,29 @@ def us_bills_input():
 def ny_bills_output():
     # pull 'subject' from input field and store it
     subject = request.args.get('subject')
+    offset = request.args.get('offset')
+    if (offset is None or int(offset) < 0):
+        offset = 0
+    limit = 20
     # just select the bills for which
     # have the subject that the user inputs
 
+    score_col = cfg['score_column']
     q_str = """
-    SELECT nyb.bill_num, nyb.bill_name, ts.logistic
+    SELECT nyb.bill_num, nyb.bill_name, ts.{0}
     FROM ny_score as ts
     INNER JOIN ny_bills as nyb
     ON nyb.bill_num=ts.bill_num
-    WHERE ts.subject={0}
-    AND ts.logistic IS NOT NULL
-    ORDER BY ts.logistic DESC
-    LIMIT 100
+    WHERE ts.subject={1}
+    AND ts.{0} IS NOT NULL
+    ORDER BY ts.{0} DESC
+    LIMIT {2}
+    OFFSET {3}
     """
-    q_fill = q_str.format(subject)
+    q_fill = q_str.format(score_col, subject, limit, offset)
     query_results = pd.read_sql_query(q_fill, con)
 
-    bills = formatted_query(query_results, 'logistic')
+    bills = formatted_query(query_results, score_col)
     # bills = []
     # for i in range(0, query_results.shape[0]):
     #    bills.append(dict(bill_num=query_results.iloc[i]['bill_num'],
@@ -67,7 +73,7 @@ def ny_bills_output():
                        "'Securities'": 'Securities',
                        "'Bank_accounts_deposits_and_capital'": 'Banking'}
     return render_template("results.html", bills=bills, cdict=conversion_dict,
-                           subject=subject)
+                           subject=subject, offset=offset)
 
 
 @app.route('/us_bills_output')
