035d2b909134addbced4b7f9140ccced1395dd20
joel <joel.m.piper@gmail.com>
2024-04-16 09:17:11 -0400

    debugging install script

--- deployment/ec2_startup.sh
+++ deployment/ec2_startup.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y git apt-transport-https ca-certificates curl software-properties-common awscli
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-compose
-sudo usermod -aG docker ${USER}
+set -xe
 
-mkdir /home/ubuntu/repos
-# Clone your project
-git clone https://github.com/joelmpiper/bill_taxonomy.git /home/ubuntu/repos/bill_taxonomy
-cd /home/ubuntu/repos/bill_taxonomy/deployment
+# Clean yum cache
+sudo yum clean all
+
+# Update system
+sudo yum update -y
+
+# Install necessary packages, resolving conflicts if necessary
+sudo yum install -y git aws-cli --allowerasing
+sudo yum install -y curl --exclude=curl-minimal*
+
+# Update and install necessary packages
+sudo amazon-linux-extras install docker -y
+sudo service docker start
+sudo systemctl enable docker
+sudo usermod -a -G docker ec2-user
+
+# Setup directory for repository and clone
+mkdir -p /home/ec2-user/repos
+cd /home/ec2-user/repos
+git clone https://github.com/joelmpiper/bill_taxonomy.git
+cd bill_taxonomy/deployment 
 
 # Set environment variables
 export LOCAL=false
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Run Docker Compose
 docker-compose up -d
