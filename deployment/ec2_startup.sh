ccdc4616ca8364245d78b5dacde334af246556a9
joel <joel.m.piper@gmail.com>
2024-04-22 09:55:17 -0400

    remove double install for docker-compose

--- deployment/ec2_startup.sh
+++ deployment/ec2_startup.sh
@@ -19,26 +19,26 @@ sudo chmod +x /usr/local/bin/yq
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Use newgrp to apply the group change immediately
-newgrp docker <<EONG
-
-# Setup virtual environment for Docker Compose
 mkdir -p /home/ec2-user/repos
-virtualenv /home/ec2-user/docker-compose-env
-source /home/ec2-user/docker-compose-env/bin/activate
-
-# Install Docker Compose
-pip install docker-compose
-
 # Clone repository
 cd /home/ec2-user/repos
 git clone https://github.com/joelmpiper/bill_taxonomy.git
 cd bill_taxonomy/deployment
+# Use newgrp to apply the group change immediately
+newgrp docker <<EONG
+
+# Setup virtual environment for Docker Compose
+# virtualenv /home/ec2-user/docker-compose-env
+# source /home/ec2-user/docker-compose-env/bin/activate
+
+# Install Docker Compose
+# pip install docker-compose
+
 
 # Run Docker Compose
 ./bootstrap.sh
 
 # Deactivate virtual environment
-deactivate
+# deactivate
 
 EONG
